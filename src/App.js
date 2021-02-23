f5692a22e9b1407f1e9c654f6bfe72d1222019b6
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-24 02:35:42 +0600

    Create Component

--- src/App.js
+++ src/App.js
@@ -7,24 +7,36 @@ function App() {
     color: "red",
     backgroundColor: "green"
   }
+  const names = ["Rayhan", "Chandan", "Rajvhi", "Shaiful"]
   return (
     <div className="App">
       <header className="App-header">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <div className="row">
+          <div className="col-md-3 col-lg-4">
+            <Person name={names[0]}></Person>
+          </div>
+          <div className="col-md-3 col-lg-4">
+            <Person name={names[1]}></Person>
+          </div>
+          <div className="col-md-3 col-lg-4">
+            <Person name={names[2]}></Person>
+          </div>
+          <div className="col-md-3 col-lg-4">
+            <Person name={names[3]}></Person>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 var nameStyle = {
-  color : "green"
+  color: "green"
 }
-function Person() {
+function Person(props) {
   return (
-    <div style={{ backgroundColor: "goldenrod", borderRadius: "20px", padding: "10px",margin: "10px" }}>
+    <div style={{ backgroundColor: "goldenrod", borderRadius: "20px", padding: "10px", margin: "10px" }}>
       <h3>Hello <span style={{ color: 'red' }}>Mr,</span></h3>
-      <h2 style={nameStyle}>Nur Mohammad Rayhan</h2>
+      <h2 style={nameStyle}>{props.name}</h2>
       <div>
         <button class="btn btn-primary">Details</button>
       </div>
