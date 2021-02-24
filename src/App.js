734f7b54ad0de488117e157e30c967a7d03aee72
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-24 16:31:16 +0600

    Dynamic Card System

--- src/App.js
+++ src/App.js
@@ -4,33 +4,39 @@ const products = [
   { name: "Photoshop", price: "$99.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
   { name: "Illustrator", price: "$45.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
   { name: "PDF Reader", price: "$35.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
-  { name: "Adobe UX", price: "$85.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." }
+  { name: "Adobe UX", price: "$85.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "Premiere Pro", price: "$75.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "Premiere Rush", price: "$65.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "After Effects", price: "$85.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "Dreamweaver", price: "$85.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
 ]
 
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="row justify-content-center">
-          <div className="col-3">
-            <ProductCart product={products[0]}></ProductCart>
-          </div>
-          <div className="col-3">
-            <ProductCart product={products[1]}></ProductCart>
-          </div>
-          <div className="col-3">
-            <ProductCart product={products[2]}></ProductCart>
-          </div>
-          <div className="col-3">
-            <ProductCart product={products[3]}></ProductCart>
-          </div>
+        <div className="row justify-content-center container">
+          {
+            products.map(product => {
+              return (
+
+                <div className="col-3">
+                  <ProductCart product={product}></ProductCart>
+                </div>
+
+              )
+            })
+          }
+        </div>
+        <div>
+          <ul>
+            {
+              products.map(product => {
+                return <li>{product.name}</li>
+              })
+            }
+          </ul>
         </div>
       </header>
     </div>
@@ -43,9 +49,10 @@ function ProductCart(props) {
     border: "1px solid grey",
     borderRadius: "5px",
     backgroundColor: "lightgray",
-    height: "250px",
-    width: "300px",
-    color: "black"
+    height: "300px",
+    width: "250px",
+    color: "black",
+    marginTop: "10px"
   }
 
   return (
