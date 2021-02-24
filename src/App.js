c1aae5cf4682ada98f53f4dc0d1204bbc190edd7
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-24 15:41:47 +0600

    Component Same ui Diffrent Data

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
 import './App.css';
 
-
-const products =[
-  {name : "Photoshop", price: "$99.90"},
-  {name : "Illustrator", price: "$45.90"},
-  {name : "PDF Reader", price: "$35.90"}
+const products = [
+  { name: "Photoshop", price: "$99.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "Illustrator", price: "$45.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "PDF Reader", price: "$35.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." },
+  { name: "Adobe UX", price: "$85.90", discription: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, officia." }
 ]
 
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+
 function App() {
-  var style = {
-    color: "red",
-    backgroundColor: "green"
-  }
-  const names = ["Rayhan", "Chandan", "Rajvhi", "Shaiful"]
   return (
     <div className="App">
       <header className="App-header">
-        <ProductCart product={products[0]}></ProductCart>
-        <ProductCart product={products[1]}></ProductCart>
-        <ProductCart product={products[2]}></ProductCart>
+        <div className="row justify-content-center">
+          <div className="col-3">
+            <ProductCart product={products[0]}></ProductCart>
+          </div>
+          <div className="col-3">
+            <ProductCart product={products[1]}></ProductCart>
+          </div>
+          <div className="col-3">
+            <ProductCart product={products[2]}></ProductCart>
+          </div>
+          <div className="col-3">
+            <ProductCart product={products[3]}></ProductCart>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
-var nameStyle = {
-  color: "green"
-}
 
-function Person(props) {
-  return (
-    <div style={{ backgroundColor: "goldenrod", borderRadius: "20px", padding: "10px", margin: "10px" }}>
-      <h3>Hello <span style={{ color: 'red' }}>Mr,</span></h3>
-      <h2 style={nameStyle}>{props.name}</h2>
-      <div>
-        <button class="btn btn-primary">Details</button>
-      </div>
-    </div>
-
-  );
-}
 
 function ProductCart(props) {
   const productStyle = {
-    border:"1px solid grey",
+    border: "1px solid grey",
     borderRadius: "5px",
     backgroundColor: "lightgray",
-    height: "200px",
-    width: "300px"
-
+    height: "250px",
+    width: "300px",
+    color: "black"
   }
-  let {Name, Price} = props.product;
+
   return (
     <div style={productStyle}>
-      <h3>Name:{Name} </h3>
-      <h2>Price:{Price} </h2>
-      <p>Product Details: </p>
+      <h3>{props.product.name} </h3>
+      <h2>{props.product.price} </h2>
+      <p>{props.product.discription} </p>
       <button>buy now</button>
     </div>
   )
